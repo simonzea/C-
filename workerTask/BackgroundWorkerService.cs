9ff8a6c5389f7fc7b747725dc67afe706177ca79
Simon Zea <szeag@unal.edu.co>
2023-09-20 13:23:17 -0500

    C# worker commit

--- workerTask/BackgroundWorkerService.cs
+++ workerTask/BackgroundWorkerService.cs
@@ -1,5 +1,10 @@
 
 namespace Namespace;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using TimeZoneConverter;
 public class BackgroundWorkerService : BackgroundService
 {
     readonly ILogger<BackgroundWorkerService> _logger;
@@ -10,10 +15,37 @@ public class BackgroundWorkerService : BackgroundService
     
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
+
+        List<string> cities = new List<string>();
+
+        cities.Add("Bogota");
+        cities.Add("Central Time (US & Canada)");
+        cities.Add("Buenos Aires");
+        cities.Add("Eastern Time (US & Canada)");
+        cities.Add("London");
+
+        ReadOnlyCollection<string> readOnlyCities =
+            new ReadOnlyCollection<string>(cities);
+
+        foreach( string dinosaur in readOnlyCities )
+        {
+            Console.WriteLine(dinosaur);
+        }
+
         while(!stoppingToken.IsCancellationRequested){
-            _logger.LogInformation("Worker Runing at: {time}", DateTimeOffset.Now );
-            _logger.LogInformation("Worker Runing at: {time}", DateTimeOffset.Now );
-            _logger.LogInformation("Worker Runing at: {time}", DateTimeOffset.Now );
+            DateTime timeUtc = DateTime.UtcNow;
+            
+
+            foreach( string city in readOnlyCities )
+        {
+            TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById(TZConvert.RailsToWindows(city));
+            DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);
+            _logger.LogInformation("City: {city}", city );
+            _logger.LogInformation("TimeZone: {timeZone}", TZConvert.RailsToIana(city) );
+            _logger.LogInformation("Time: {time}", cstTime.ToString("yyyy-MM-dd'T'HH:mm:ss.FFFzzz", System.Globalization.CultureInfo.InvariantCulture) );
+        }
+            
+            
             await Task.Delay(30000, stoppingToken);
         }
     }
